8d661c67b33c6d666f9443e1527c363f1ce62cb2
Brian Bartholomew <me@bcbrian.com>
2019-09-27 12:03:14 -0500

    Update canary.test.js

--- test/canary.test.js
+++ test/canary.test.js
@@ -2,6 +2,6 @@ describe("canary test", function() {
   // A "canary" test is one we set up to always pass
   // This can help us ensure our testing suite is set up correctly before writing real tests
   it("should pass this canary test", function() {
-    expect(true).toBeTrue;
+    const a = expect(true).toBeTrue();
   });
 });
